c2c6185dba85e51103425efc210e57be17240b6f
Pierce Lamb <plamb@Pierces-MacBook-Pro.local>
2018-06-22 15:01:08 -0700

    indexOutOfBounds

--- src/main/scala/Converters/AccessLogToRowsConverter.scala
+++ src/main/scala/Converters/AccessLogToRowsConverter.scala
@@ -9,12 +9,19 @@ import org.apache.spark.sql.Row
 class AccessLogToRowsConverter extends StreamToRowsConverter with Serializable {
 
   //what an access log looks like:
- //14:40:33.251 [application-akka.actor.default-dispatcher-6] INFO  access - 0:0:0:0:0:0:0:1 - [22/Jun/2018:14:40:33 -0700] - "GET /assets/images/favicon/favicon.ico" - 200 - 22382
+ //"""14:40:33.251 [application-akka.actor.default-dispatcher-6] INFO  access - 0:0:0:0:0:0:0:1 - [22/Jun/2018:14:40:33 -0700] - "GET /assets/images/favicon/favicon.ico" - 200 - 22382"""
 
   override def toRows(message: Any): Seq[Row] = {
     val log = message.asInstanceOf[String]
     val field = log.split(" - ")
-    Seq(Row.fromSeq(Seq(
+    Seq(
+      Row.fromSeq(
+        Seq(
+          Option(field(1)).getOrElse(""), //ip
+          Option(field(2)).getOrElse(""), //datetime
+          Option(field(3)).getOrElse(""), //header
+          Option(field(4)).getOrElse(""), //response
+          Option(field(5)).getOrElse("") //size
 
     )))
   }
